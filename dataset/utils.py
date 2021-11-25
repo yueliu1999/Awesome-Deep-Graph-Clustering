b4f3fe675badfdad113774f9354d91f0c626f545
Yue Liu <41297969+YueLiu-coder@users.noreply.github.com>
2021-11-25 15:36:12 +0800

    add the non-graph data loader

--- dataset/utils.py
+++ dataset/utils.py
@@ -26,7 +26,7 @@ def load_graph_data(dataset_name, show_details=False):
     adj = np.load(load_path+"_adj.npy", allow_pickle=True)
     if show_details:
         print("++++++++++++++++++++++++++++++")
-        print("------details of dataset------")
+        print("---details of graph dataset---")
         print("++++++++++++++++++++++++++++++")
         print("dataset name:   ", dataset_name)
         print("feature shape:  ", feat.shape)
@@ -42,6 +42,40 @@ def load_graph_data(dataset_name, show_details=False):
     return feat, label, adj
 
 
+def load_data(dataset_name, show_details=False):
+    """
+    load non-graph data
+    :param dataset_name: the name of the dataset
+    :param show_details: if show the details of dataset
+    - dataset name
+    - features' shape
+    - labels' shape
+    - category num
+    - category distribution
+    :return: the features and labels
+    """
+    load_path = dataset_name+"/"+dataset_name
+    feat = np.load(load_path+"_feat.npy", allow_pickle=True)
+    label = np.load(load_path+"_label.npy", allow_pickle=True)
+    if show_details:
+        print("++++++++++++++++++++++++++++++")
+        print("------details of dataset------")
+        print("++++++++++++++++++++++++++++++")
+        print("dataset name:   ", dataset_name)
+        print("feature shape:  ", feat.shape)
+        print("label shape:    ", label.shape)
+        print("category num:   ", max(label)+1)
+        print("category distribution: ")
+        for i in range(max(label)+1):
+            print("label", i, end=":")
+            print(len(label[np.where(label == i)]))
+        print("++++++++++++++++++++++++++++++")
+    return feat, label
+
+
 if __name__ == '__main__':
-    dataset = "citeseer"
-    X, y, A = load_graph_data(dataset, show_details=True)
+    graph_dataset = "dblp"
+    X, y, A = load_graph_data(graph_dataset, show_details=True)
+
+    # non_graph_dataset = "hhar"
+    # X, y = load_data(non_graph_dataset, show_details=True)
