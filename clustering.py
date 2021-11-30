4572a0e54f49a9c79625a4b27fc849f3f00fb651
Yue Liu <41297969+YueLiu-coder@users.noreply.github.com>
2021-11-30 12:02:13 +0800

    add evaluation and k_means function

--- clustering.py
+++ clustering.py
@@ -6,6 +6,11 @@
 import torch
 import numpy as np
 import random
+from sklearn.metrics import adjusted_rand_score as ari_score
+from sklearn.metrics.cluster import normalized_mutual_info_score as nmi_score
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.cluster import KMeans
+from munkres import Munkres
 
 
 def setup_seed(seed):
@@ -25,7 +30,72 @@ def setup_seed(seed):
     return None
 
 
-def eva():
+def evaluation(y_true, y_pred):
+    """
+    evaluate the clustering performance
+    :param y_true: ground truth
+    :param y_pred: prediction
+    :return:
+    - accuracy
+    - normalized mutual information
+    - adjust rand index
+    - f1 score
+    """
+    nmi = nmi_score(y_true, y_pred, average_method='arithmetic')
+    ari = ari_score(y_true, y_pred)
 
-    return None
+    y_true = y_true - np.min(y_true)
+    l1 = list(set(y_true))
+    num_class1 = len(l1)
+    l2 = list(set(y_pred))
+    num_class2 = len(l2)
+    ind = 0
+    if num_class1 != num_class2:
+        for i in l1:
+            if i in l2:
+                pass
+            else:
+                y_pred[ind] = i
+                ind += 1
+    l2 = list(set(y_pred))
+    num_class2 = len(l2)
+    if num_class1 != num_class2:
+        print('error')
+        return
+    cost = np.zeros((num_class1, num_class2), dtype=int)
+    for i, c1 in enumerate(l1):
+        mps = [i1 for i1, e1 in enumerate(y_true) if e1 == c1]
+        for j, c2 in enumerate(l2):
+            mps_d = [i1 for i1 in mps if y_pred[i1] == c2]
+            cost[i][j] = len(mps_d)
+    m = Munkres()
+    cost = cost.__neg__().tolist()
+    indexes = m.compute(cost)
+    new_predict = np.zeros(len(y_pred))
+    for i, c in enumerate(l1):
+        c2 = l2[indexes[i][1]]
+        ai = [ind for ind, elm in enumerate(y_pred) if elm == c2]
+        new_predict[ai] = c
+    acc = accuracy_score(y_true, new_predict)
+    f1 = f1_score(y_true, new_predict, average='macro')
 
+    return acc, nmi, ari, f1
+
+
+def k_means(embedding, k, y_true):
+    """
+    K-means algorithm
+    :param embedding: embedding of clustering
+    :param k: hyper-parameter in K-means
+    :param y_true: ground truth
+    :return:
+    - acc
+    - nmi
+    - ari
+    - f1
+    - cluster centers
+    """
+    model = KMeans(n_clusters=k, n_init=20)
+    cluster_id = model.fit_predict(embedding)
+    acc, nmi, ari, f1 = evaluation(y_true, cluster_id)
+    return acc, nmi, ari, f1, model.cluster_centers_
