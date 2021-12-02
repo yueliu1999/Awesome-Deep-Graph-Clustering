f737cf56b92f2da5d5b5fef5a3e9539c47ef4211
Yue Liu <41297969+YueLiu-coder@users.noreply.github.com>
2021-12-02 21:16:17 +0800

    add t_sne function

--- main.py
+++ main.py
@@ -5,27 +5,29 @@
 
 from utils import load_graph_data, load_data, construct_graph, normalize_adj
 from clustering import setup_seed
+from visualization import t_sne
 
 if __name__ == '__main__':
     # fix the random seed
     setup_seed(0)
 
-    # dataset_name = "dblp"
-    # dataset_type = "graph"
+    dataset_name = "acm"
+    dataset_type = "graph"
 
-    dataset_name = "hhar"
-    dataset_type = "non_graph"
+    # non_graph_dataset = "hhar"
+    # dataset_type = "non_graph"
 
     if dataset_type == "graph":
         # graph dataset
-        graph_dataset = "dblp"
-        X, y, A = load_graph_data(graph_dataset, show_details=True)
+        X, y, A = load_graph_data(dataset_name, show_details=True)
 
     if dataset_type == "non_graph":
         # non graph dataset
-        non_graph_dataset = "hhar"
-        X, y = load_data(non_graph_dataset, show_details=True)
+        X, y = load_data(dataset_name, show_details=True)
         A = construct_graph(X, k=5)
 
     # normalize the adj
     norm_A = normalize_adj(A, self_loop=True, symmetry=True)
+
+    # t-SNE
+    t_sne(X, y)
--- visualization.py
+++ visualization.py
@@ -3,64 +3,44 @@ import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 
 
-def plot_embedding(embeds, labels, title, dataset):
+def t_sne(embeds, labels, sample_num=2000, show_fig=True):
     """
+    visualize embedding by t-SNE algorithm
     :param embeds: embedding of the data
     :param labels: labels
-    :param title:图像标题
-    :return:图像
+    :param sample_num: the num of samples
+    :param show_fig: if show the figure
+    :return: figure
     """
-    mean = np.mean(embeds, axis=0)
-    std = np.std(embeds, axis=0)
 
-    data1 = embeds
-    data = []
-    for i in range(len(data1)):
-        if (data1[i]-mean < 3 * std).all():
-            data.append(data1[i])
-    data = np.array(data)
-    x_min, x_max = np.min(data, 0), np.max(data, 0)
-    data = (data - x_min) / (x_max - x_min)
-    
-    fig = plt.figure()  # 创建图形实例
-    ax = plt.subplot(111)  # 创建子图
-    # 遍历所有样本
-    for i in range(data.shape[0]):
-        # 在图中为每个数据点画出标签
-        if dataset == "cite":
-            labels[i] -= 1
-            plt.text(data[i, 0], data[i, 1], str(labels[i]), color=plt.cm.Set1(labels[i] % 7),
-                     fontdict={'weight': 'bold', 'size': 7})
-        else:
-            plt.text(data[i, 0], data[i, 1], str(labels[i]), color=plt.cm.Set1(labels[i] % 7),
-                     fontdict={'weight': 'bold', 'size': 7})
+    # sampling
+    sample_index = np.random.randint(0, embeds.shape[0], sample_num)
+    sample_embeds = embeds[sample_index]
+
+    # t-SNE
+    ts = TSNE(n_components=2, init='pca', random_state=0)
+    ts_embeds = ts.fit_transform(sample_embeds[:, :])
+
+    # remove outlier
+    mean, std = np.mean(ts_embeds, axis=0), np.std(ts_embeds, axis=0)
+    for i in range(len(ts_embeds)):
+        if (ts_embeds[i] - mean < 3 * std).all():
+            np.delete(ts_embeds, i)
+
+    # normalization
+    x_min, x_max = np.min(ts_embeds, 0), np.max(ts_embeds, 0)
+    norm_ts_embeds = (ts_embeds - x_min) / (x_max - x_min)
+
+    # plot
+    fig = plt.figure()
+    for i in range(norm_ts_embeds.shape[0]):
+        plt.text(norm_ts_embeds[i, 0], norm_ts_embeds[i, 1], str(labels[i]), color=plt.cm.Set1(labels[i] % max(labels)),
+                 fontdict={'weight': 'bold', 'size': max(labels)})
     plt.xticks([])
     plt.yticks([])
-    plt.title(title, fontsize=14)
+    plt.title('t-SNE', fontsize=14)
     plt.axis('off')
+    if show_fig:
+        plt.show()
+
     return fig
-
-if __name__ == '__main__':
-    for dataset in ["acm", "dblp", "cite", "amap", "pubmed", "corafull", "amac"]:
-        if dataset in ["amap", "amac"]:
-            x = np.load("./data/{}/feat.npy".format(dataset))
-            y = np.load("./data/{}/labels.npy".format(dataset))
-        else:
-            x = np.loadtxt("data/{}/{}.txt".format(dataset, dataset), dtype=float)
-            y = np.loadtxt("data/{}/{}_label.txt".format(dataset, dataset), dtype=int)
-        np.random.seed(42)
-        sample_index = np.random.randint(0, x.shape[0], 2000)
-        x = x[sample_index]
-        y = y[sample_index]
-        ts = TSNE(n_components=2, init='pca', random_state=0)
-        print("Transforming:")
-        result = ts.fit_transform(x[:, :])
-        print("OK")
-        # 调用函数，绘制图像
-        fig = plot_embedding(result, y, '', dataset)
-        # 显示图像
-
-        plt.savefig("./result/{}.png".format(dataset))
-        # plt.show()
-
-
